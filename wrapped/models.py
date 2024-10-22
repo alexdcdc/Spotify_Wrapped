5a276132809900750607e42aa78d7fb10c840f88
alexdcdc <alexdcdc@gmail.com>
2024-10-21 21:08:53 -0400

    add custom user model + user manager

--- wrapped/models.py
+++ wrapped/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
 
 # Create your models here.
