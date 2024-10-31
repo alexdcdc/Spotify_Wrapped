44ca5c72f11df3528d2ab744528b63d0849c339a
alexdcdc <alexdcdc@gmail.com>
2024-10-31 11:31:41 -0400

    fix lint issues w/ js

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,32 +1,32 @@
-import {useState, useEffect} from 'react'
-function Dashboard() {
-    const [firstName, setFirstName] = useState("");
+import { useState, useEffect } from 'react'
+function Dashboard () {
+  const [firstName, setFirstName] = useState('')
 
-    const getUserData = async () => {
-        const url = "http://localhost:8000/api/user";
-        const payload = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Token ' + localStorage.getItem('token'),
-            }
-        }
-        const response = await fetch(url, payload);
-        if (!response.ok) {
-            return Promise.reject("Unable to get user data");
-        }
-
-        const user = await response.json();
-        setFirstName(user.first_name);
+  const getUserData = async () => {
+    const url = 'http://localhost:8000/api/user'
+    const payload = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Token ' + localStorage.getItem('token')
+      }
+    }
+    const response = await fetch(url, payload)
+    if (!response.ok) {
+      return Promise.reject(new Error('Unable to get user data'))
     }
 
-    useEffect(() => {getUserData()}, [])
+    const user = await response.json()
+    setFirstName(user.first_name)
+  }
 
-    return (
-        <div>
-            <p>Welcome to the dashboard, {firstName}.</p>
-        </div>
-    )
+  useEffect(() => { getUserData() }, [])
+
+  return (
+    <div>
+      <p>Welcome to the dashboard, {firstName}.</p>
+    </div>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
