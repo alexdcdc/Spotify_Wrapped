cb39f0b22da112d32ee299e9bb1c3fb82bfc0799
alexdcdc <alexdcdc@gmail.com>
2024-11-18 16:50:56 -0500

    quick logout functionality

--- frontend/src/lib/auth.js
+++ frontend/src/lib/auth.js
@@ -1,3 +1,12 @@
+
 function isAuthenticated() {
   return !!sessionStorage.getItem("token");
 }
+
+function logout() {
+  localStorage.removeItem('token')
+  window.location.href="http://localhost:3000"
+}
+
+
+export { isAuthenticated }
