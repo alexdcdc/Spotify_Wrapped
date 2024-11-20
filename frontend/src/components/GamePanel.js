a5c82d9ad32eaf979d3d95ff153a340ada9c2f67
alexdcdc <alexdcdc@gmail.com>
2024-11-20 11:17:41 -0500

    start game panel

--- frontend/src/components/GamePanel.js
+++ frontend/src/components/GamePanel.js
@@ -1,5 +1,26 @@
-function GamePanel () {
-  return (<p>Game Panel TBA</p>)
+function GamePanel ({data}) {
+  if (!data.correct || !data.choices) {
+    return (<p>Invalid data passed into game panel</p>)
+  }
+
+  const correctTrack = data.correct
+  const trackChoices = data.choices
+
+  const checkAnswer = (e) => {
+    console.log()
+  }
+
+
+
+
+  return (
+    <form onSubmit = {checkAnswer}>
+      <datalist>
+        {trackChoices.map((track) => <option>{track.name}</option>)}
+      </datalist>
+      <input type="submit">Submit</input>
+    </form>
+  )
 }
 
 export default GamePanel
