51b34cefb2c58fed7f25dea65ab8228b6defdaaf
alexdcdc <alexdcdc@gmail.com>
2024-11-26 02:45:36 -0500

    formatting fixes

--- frontend/src/components/panels/PreGamePanel.js
+++ frontend/src/components/panels/PreGamePanel.js
@@ -1,7 +1,7 @@
-import "./PreGamePanel.css"
-function PreGamePanel() {
+import './PreGamePanel.css'
+function PreGamePanel () {
   return (
-    <div className="wrapped-background pre-game-wrapper">
+    <div className='wrapped-background pre-game-wrapper'>
       <h1>How well do you know your music? Take this next quiz and find out!</h1>
     </div>
   )
