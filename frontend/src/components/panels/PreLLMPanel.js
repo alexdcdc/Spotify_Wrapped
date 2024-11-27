6915c56581361ee0ec74aea24ccec10dfa6adcc2
alexdcdc <alexdcdc@gmail.com>
2024-11-26 23:10:50 -0500

    preLLM panel, back to dashboard button on last panel, dance panel changes

--- frontend/src/components/panels/PreLLMPanel.js
+++ frontend/src/components/panels/PreLLMPanel.js
@@ -1,5 +1,11 @@
-function PreLLMPanel () {
-  return (<p>Pre LLM TBA</p>)
+import "./PreLLMPanel.css"
+
+function PreLLMPanel() {
+  return (
+    <div className='wrapped-background pre-llm-wrapper'>
+      <h1>Now, let's look at a personality snapshot of people with your music tastes.</h1>
+    </div>
+  )
 }
 
 export default PreLLMPanel
