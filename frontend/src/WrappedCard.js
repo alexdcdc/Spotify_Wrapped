b2f5eb6a5b01230e9e4cdcc6c2d7b0b41b454a73
anyeelii <anyee.li05@gmail.com>
2024-12-01 22:17:47 -0500

    added delete button

--- frontend/src/WrappedCard.js
+++ frontend/src/WrappedCard.js
@@ -1,18 +1,36 @@
 import './WrappedCard.css'
 import {useNavigate} from 'react-router-dom'
+import { del } from './lib/requests'
 
-function WrappedCard({id, name, dateCreated, color}) {
+function WrappedCard({id, name, dateCreated, color, onDelete}) {
   const navigate = useNavigate()
+
+  const handleDelete = async () => {
+    const url = `http://localhost:8000/api/wrapped/${id}`
+
+    const response = await del(url, {}, true)
+
+    if (response.ok) {
+      onDelete(id)  // Call the delete handler from parent component
+    } else {
+      console.error('Failed to delete card')
+    }
+  }
+
   return (
-    <div className='card' onClick={() => {
-      navigate('/wrapped/' + id)
-    }}>
-      <div className="image-placeholder" style={{backgroundColor: "#" + color}}/>
+    <div className='card'>
+      <div className="image-placeholder" style={{ backgroundColor: "#" + color }}/>
       <h3>{name}</h3>
       <p>Created {dateCreated.toDateString()}</p>
-    </div>
+      <div className="button-container">
+        <button className="view-details-button" onClick={() => navigate('/wrapped/' + id)}>View Wrapped</button>
+        {/* View details */}
+        <button className="delete-button" onClick={handleDelete}>Delete</button>
+        {/* Delete button */}
+      </div>
+      </div>
 
-  )
-}
+      )
+      }
 
-export default WrappedCard
+      export default WrappedCard
